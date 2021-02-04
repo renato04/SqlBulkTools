7ff218d6de9186fcc591a2821b45d3292850b140
Renato Ramos <ramos.renato@outlook.com>
2021-02-04 10:11:43 -0300

    Including .net core project

---
+++ SqlBulkTools.Core/ITransaction.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace SqlBulkTools
+{
+    internal interface ITransaction
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
+        void CommitTransaction(SqlConnection connection);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        Task CommitTransactionAsync(SqlConnection connection);
+    }
+}
