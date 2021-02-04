728ad78a202baf0614ff3d00219568486e14dd71
Renato <ramos.renato@outlook.com>
2021-02-04 11:37:11 -0300

    adding .net standard class library

---
+++ SqlBulkTools.NetStandard/BulkOperations.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBulkTools
+{
+    public class BulkOperations : IBulkOperations
+    {
+        private ITransaction _transaction;
+        private const string SourceAlias = "Source";
+        private const string TargetAlias = "Target";
+
+        internal void SetBulkExt(ITransaction transaction)
+        {
+            _transaction = transaction;
+        }
+
+        /// <summary>
+        /// Commits a transaction to database. A valid setup must exist for operation to be 
+        /// successful. 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public void CommitTransaction(SqlConnection connection)
+        {
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+
+            if (_transaction == null)
+                throw new InvalidOperationException("No setup found. Use the Setup method to build a new setup then try again.");
+
+            _transaction.CommitTransaction(connection: connection);
+
+        }
+
+
+        /// <summary>
+        /// Commits a transaction to database. A valid setup must exist for operation to be 
+        /// successful. 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="InvalidOperationException"></exception>
+        public async Task CommitTransactionAsync(SqlConnection connection)
+        {
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+
+            if (_transaction == null)
+                throw new InvalidOperationException("No setup found. Use the Setup method to build a new setup then try again.");
+
+            await _transaction.CommitTransactionAsync(connection: connection);
+        }
+
+        /// <summary>
+        /// Each transaction requires a valid setup. Examples available at: https://github.com/gtaylor44/SqlBulkTools 
+        /// </summary>
+        /// <typeparam name="T">The type of collection to be used.</typeparam>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public CollectionSelect<T> Setup<T>(Func<Setup<T>, CollectionSelect<T>> list)
+        {
+            CollectionSelect<T> tableSelect = list(new Setup<T>(SourceAlias, TargetAlias, this));
+            return tableSelect;
+        }
+    }
+}
---
+++ SqlBulkTools.NetStandard/IBulkOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlBulkTools
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IBulkOperations
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
+        void CommitTransaction(SqlConnection connection);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="connection"></param>
+        /// <returns></returns>
+        Task CommitTransactionAsync(SqlConnection connection);
+        
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        CollectionSelect<T> Setup<T>(Func<Setup<T>, CollectionSelect<T>> list);
+    }
+}
